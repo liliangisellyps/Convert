7e61fc1650bc0caa1c05ba46aa5de03c04630f06
Lilian Giselly <lgps@ic.ufal.br>
2020-07-29 17:26:58 -0300

    Modal funcionando

---
+++ public/scripts/convert.js
@@ -0,0 +1,8 @@
+const number = document.querySelector([name=number])
+
+const binary = parseInt(number, 2)
+const octal = parseInt(number, 8)
+const decimal = parseInt(number, 10)
+const hexa = parseInt(number, 16)
+
+console.log(binary, octal, decimal, hexa)
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,23 @@
-const buttonConvert = document.getElementById(convert_button)
+const buttonConvert = document.querySelector("#page-home main #convert_button")
+const modal = document.querySelector("#modal")
+const close = document.querySelector("#modal .header a")
 
 buttonConvert.addEventListener("click", () => {
     modal.classList.remove("hide")
-    console.log("ola")
 })
+
+close.addEventListener("click", () => {
+    modal.classList.add("hide")
+})
+
+
+// const buttonCopy = document.querySelector(#modal #copy_button)
+// document.getElementById(buttonCopy).addEventListener(“click”, function(){
+
+//     document.getElementById(“texto”).select();
+    
+//     document.execCommand(‘copy’);
+    
+//     });
 
 console.log("ola")
